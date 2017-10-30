f867326e4bb59ffa9002c6d26119f1ed84fe3750
Amie Corso <acorso@cs.uoregon.edu>
2017-10-30 10:02:08 -0700

    need to figure out how to store channels

--- server.c
+++ server.c
@@ -6,21 +6,52 @@ Fall 2017
 
 /*
 
-To make the project seem more real, but not without the added complexity of TCP, DuckChat uses UDP to communicate. While UDP is not reliable, we can run the client and server on the same machine and not worry about dropping packets or out of order packets.
-
 The server takes two arguments: the host address to bind to and the port number. The host address can be either 'localhost', the IP host name of the machine that the server is running on, or the IP address of an interface connected to the machine. Once the server is running, clients may use the host name and port to connect to the server. Note that if you use 'localhost', you will not be able to connect to the server from another machine, but you also do not have to worry about dropped packets.
-
 The server does not need to directly interact with the user in any way. However, it is strongly recommended that the server outputs debugging messages to the console. For example, each time the server receives a message from a client, it can output a line describing the contents of the message and who it is from using the following format: [channel][user][message] where message denotes a command and its parameters (if any).
-
-The server's primary job is to deliver messages from a user X to all users on X's active channel. To accomplish this, the server must keep track of individual users and the channels they have subscribed to. On the flip side, the server must also track each channel and the subscribed users on it.
 
 Channel creation and deletion at server are handled implicitly. Whenever a channel has no users, it is deleted. Whenever a user joins a channel that did not exist, it is created.
 
 If the server receives a message from a user who is not logged in, the server should silently ignore the message.
 
-SKETCH OF A SERVER IN C
+1) establish server data structures
+userlist and a chanlist: structs with just HEADs to linked list of unodes and chnodes.
+unode and chnode? MAKE TWO THINGS
+users = linked list of pointers to users (should users itself be the head pointer??)
+channels = head of linked list of pointers to channels
+user = struct with:
+- username
+- socket fd
+- last-active (gettimeofday that user was last active)
+- HEAD of a linked list of channel pointers
 
-}
+channel = struct with:
+- channel name
+- USERCOUNT
+- HEAD of linked list of user pointers
+
+Server will create a ulist and a chanlist upon its establishment, and free them upon exit??
+2) figure out how to multiplex client sockets (and associate with user names)
+    ???
+
+3) write functions to handle each of the 8 possible requests
+login(uname)
+logout(uname)
+join(uname, chname))
+leave(uname, chname)
+say(uname, chname)
+list(uname, chname)
+who(uname, chname)
+keep_alive(uname) (update time)
+ --> these may call subfunctions such as 
+create_user(uname)
+create_channel(chname)
+delete_user(uname)
+delete_channel(chname)
+add_utoch(uname, chname)
+add_chtou(uname, chname)
+rm_ufromch(uname, chname)
+rm_chfromu(chname, chname)
+   --> some of these requests will package up packets of info and send to correct client
 */
 #include <stdio.h>
 #include <stdlib.h>
